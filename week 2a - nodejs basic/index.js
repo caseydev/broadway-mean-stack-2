089057f3422843aa03fa9a90e9efa72cac6af5d2
Madan Tamang <madan.tamang@agileitsolutions.net>
2017-02-28 10:09:57 +0545

    added days 3 file

--- week 2a - nodejs basic/index.js
+++ week 2a - nodejs basic/index.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
-const http = require('http');
-const math = require('mathjs');
-const express = require('express');
-const Product = require('./Product');
-console.log(math.round(math.e, 3));            // 2.718 );
+const fs = require('fs');//this is built-in module
+const http = require('http');// this is built-in module
+const math = require('mathjs'); // this is third party module : use npm install mathjs --save
+const express = require('express');// this is third party module
+const Product = require('./Product');// this is custom module class
+// log output from Mathjs module
+console.log(math.round(math.e, 3));            // 2.718
+// log output from product js
 var P1=new Product("PA",100,10);
 console.log(P1.printtotalprice());
 console.log(P1.title);
@@ -19,14 +21,16 @@ else{
   console.log(data.toString());
 }
 });
+//nodejs creating server
+// var path = url.parse(request.url).pathname;
 var server=http.createServer(function(request,response){
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("hello nodejs server");
+  response.write("hello client i ma sending response from nodejs server");
   response.end();
 });
 server.listen(3000,function (err) {
   if (err) {
-    return console.log('something bad happened', err)
+     console.log('something bad happened', err)
   }
   console.log(`server is listening on 3000`)
 })
