b4dc53bc5a170aff681d15ba6e4fa955bd7987f7
Madan Tamang <madan.tamang@agileitsolutions.net>
2017-03-02 10:03:02 +0545

    added changes

--- week 2c - node expressjs basic/server.js
+++ week 2c - node expressjs basic/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app=express();
+const viewpath=__dirname + "/views";
+console.log(__dirname) // this give absolute path
+// Get /
 app.get("/",function (req,res){
-  res.sendFile("index.html");
-})
+  res.sendFile(viewpath + "/index.html");
+});
+
 app.get("/contact",function (req,res){
-  res.sendFile("contact.html");
-})
+  res.sendFile(viewpath + "/contact.html");
+});
 app.post("/contact",function (req,res){
   res.send('Got a POST request');
-})
+});
 app.get("/about",function (req,res){
-  res.sendFile("about.html");
-})
+  res.sendFile(viewpath + "/about.html");
+});
 app.listen(3000,function(){
   console.log("app listening on port 3000!");
 });
