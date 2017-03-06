277f602314d3dfd734a5c246a0692197644df342
Madan Tamang <madan.tamang@agileitsolutions.net>
2017-03-06 20:23:34 +0545

    changes files

--- week 2d - express generator/routes/products.js
+++ week 2d - express generator/routes/products.js
@@ -1,7 +1,21 @@
-/**
- * Created by madan.tamang on 3/5/2017.
- */
+var express = require('express');
+var router = express.Router();
 
-app.get('/', function(req, res) {
+
+router.get('/', function(req, res) {
     return render("Product/index")
 });
+router.get('/details/:id', function(req, res) {
+    var product1={
+        title:"Dell pc",
+        Price:120000,
+        "Image":"dell34.jpg"
+    };
+    var product2={
+        title:"Acer pc",
+        Price:120000,
+        "Image":"acer34.jpg"
+    };
+    return render("Product/details")
+});
+module.exports=router;
